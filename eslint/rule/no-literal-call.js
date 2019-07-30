26092b5690558f8f76e44cdd0f9a2d62636c8309
ozaki <fs65ugumori@yahoo.co.jp>
2019-07-30 19:33:30 +0900

    update custom rule

--- eslint/rule/no-literal-call.js
+++ eslint/rule/no-literal-call.js
@@ -1,9 +1,31 @@
 'use strict';
 
+var LITERAL_TYPE = /^(?:Literal|ArrayExpression|ObjectExpression|TemplateLiteral)$/;
+
 module.exports = function(context) {
   return {
-    ArrayExpression: function(node) {
-      console.log('配列リテラルを発見!');
+    /**
+     * この CallExpression の呼び出し対象が Literal, ArrayExpression, ObjectExpression,
+     * TemplateLiteral, ClassExpression のいずれかだった場合、警告する。
+     *
+     * @param {ASTNode} node - チェックする CallExpression ノード。
+     */
+    CallExpression: function(node) {
+      var callee = node.callee;
+      var message = null;
+
+      // 呼び出し対象のtypeがリテラルかどうかチェックする。
+      if (LITERAL_TYPE.test(callee.type)) {
+        message = 'This is not a function.';
+      }
+      if (callee.type === 'ClassExpression') {
+        message = "Class constructors cannot be invoked without 'new'.";
+      }
+
+      // リテラルだったら警告する
+      if (message) {
+        context.report({ node: node, message: message });
+      }
     },
   };
 };
